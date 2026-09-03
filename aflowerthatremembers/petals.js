a4f9c9b80619754bb627a3fd34b36651d93a780d
Popoyyo <54846761+Popoyyo@users.noreply.github.com>
2026-09-03 15:42:22 +0800

    Add files via upload

---
+++ aflowerthatremembers/petals.js
@@ -0,0 +1,88 @@
+let openPetal = null;
+
+const flower = document.querySelector(".flower");
+
+function showPanel(number) {
+
+    const panel = document.querySelector(".panel-" + number);
+
+    if (!panel) {
+        return;
+    }
+
+    if (openPetal === number) {
+
+        panel.classList.remove("show");
+
+        flower.style.transform =
+            "translate(-50%, -50%)";
+
+        openPetal = null;
+
+        return;
+    }
+
+    if (openPetal !== null) {
+        return;
+    }
+
+    openPetal = number;
+
+    if (number === 1) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(0px, 300px)";
+    }
+
+    else if (number === 2) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(-210px, 210px)";
+    }
+
+    else if (number === 3) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(-300px, 0px)";
+    }
+
+    else if (number === 4) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(-210px, -210px)";
+    }
+
+    else if (number === 5) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(0px, -300px)";
+    }
+
+    else if (number === 6) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(210px, -210px)";
+    }
+
+    else if (number === 7) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(300px, 0px)";
+    }
+
+    else if (number === 8) {
+        flower.style.transform =
+            "translate(-50%, -50%) translate(210px, 210px)";
+    }
+
+    setTimeout(function() {
+        panel.classList.add("show");
+    }, 100);
+}
+
+
+/* CLICK THE IMAGE/PANEL */
+
+function panelClick(number) {
+
+    const textPanel = document.querySelector(".text-panel-" + number);
+
+    if (!textPanel) {
+        return;
+    }
+
+    textPanel.classList.toggle("show");
+}
